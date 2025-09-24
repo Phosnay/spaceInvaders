49e29160eb4883c1f1a96f9e98a7eec5b05d777a
Muriel Bullot <murielbullot@hotmail.fr>
2025-09-24 14:11:24 +0200

    correction et nouveau paramètres dans les fonctions

--- fonctions/defMissile.py
+++ fonctions/defMissile.py
@@ -1,4 +1,6 @@
-def missile(player):
+import pygame as pg, sys                # sys = module qui permet d'interagir avec Python (sys.exit())
+
+def missile(player, screen, WCOLOR):
     missile = pg.Rect(player.x, player.y, 25, 25)
     pg.draw.rect(screen, WCOLOR, missile)
     print("Missile créé")
