7b17def723589fb8ed09f9e6a34f954bc107d57d
Muriel Bullot <murielbullot@hotmail.fr>
2025-09-25 10:05:04 +0200

    refactoring pour l'affichage des ennemis

--- fonctions/ennemis.py
+++ fonctions/ennemis.py
@@ -7,10 +7,10 @@ def displayLigne(who, x, y, screen, SIZE, SPACE):
         ennemy = pg.Rect((x + ((SIZE + SPACE) * i)), y, SIZE, SIZE)                            # rectangle (x, y, largeur, hauteur) 
         screen.blit(who, ennemy.topleft) 
 
-def displayEnnemies(screen, WIDTH, SIZE, SPACE):
-    firstSpotX = (WIDTH - ((10 * SIZE) + (9 * SPACE))) // 2                     # // : division entière (sans reste)
-    firstSpotY = 100
-
+def displayEnnemies(screen, WIDTH, SIZE):
+    SPACE = 10
+    X = (WIDTH - ((10 * SIZE) + (9 * SPACE))) // 2                     # // : division entière (sans reste)
+    Y = 100
 
     ennemyStrong_img = pg.image.load("graph/space10.1.png").convert_alpha()     # chargement d'une image, en gardant la transparence
     ennemyStrong_img = pg.transform.scale(ennemyStrong_img, (SIZE, SIZE))
@@ -18,11 +18,12 @@ def displayEnnemies(screen, WIDTH, SIZE, SPACE):
     ennemyMedium_img = pg.transform.scale(ennemyMedium_img, (SIZE, SIZE))
     ennemyWeak_img = pg.image.load("graph/space13.1.png").convert_alpha()
     ennemyWeak_img = pg.transform.scale(ennemyWeak_img, (SIZE, SIZE))
-    displayLigne(ennemyStrong_img, firstSpotX, firstSpotY, screen, SIZE, SPACE)
-    displayLigne(ennemyMedium_img, firstSpotX, (firstSpotY + (SIZE + SPACE)) , screen, SIZE, SPACE)
-    displayLigne(ennemyMedium_img, firstSpotX, (firstSpotY + (SIZE + SPACE) * 2), screen, SIZE, SPACE)
-    displayLigne(ennemyWeak_img, firstSpotX, (firstSpotY + (SIZE + SPACE) * 3), screen, SIZE, SPACE)
-    displayLigne(ennemyWeak_img, firstSpotX, (firstSpotY + (SIZE + SPACE) * 4), screen, SIZE, SPACE)
-    displayLigne(ennemyWeak_img, firstSpotX, (firstSpotY + (SIZE + SPACE) * 5), screen, SIZE, SPACE)
+    
+    displayLigne(ennemyStrong_img, X, Y, screen, SIZE, SPACE)
+    displayLigne(ennemyMedium_img, X, (Y + (SIZE + SPACE)) , screen, SIZE, SPACE)
+    displayLigne(ennemyMedium_img, X, (Y + (SIZE + SPACE) * 2), screen, SIZE, SPACE)
+    displayLigne(ennemyWeak_img, X, (Y + (SIZE + SPACE) * 3), screen, SIZE, SPACE)
+    displayLigne(ennemyWeak_img, X, (Y + (SIZE + SPACE) * 4), screen, SIZE, SPACE)
+    displayLigne(ennemyWeak_img, X, (Y + (SIZE + SPACE) * 5), screen, SIZE, SPACE)
 
 
