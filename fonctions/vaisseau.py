912a9489d46f746f8d754b5c8314966551895920
Phosnay <nils.romano@gmail.com>
2025-09-26 11:51:39 +0200

    Ajout tir missile (problème vaiseau joueur un peu bug)

--- fonctions/vaisseau.py
+++ fonctions/vaisseau.py
@@ -1,25 +1,6 @@
 import pygame as pg, sys                # sys = module qui permet d'interagir avec Python (sys.exit())
 import time
 
-def missile(player, screen, WCOLOR):
-    missile = pg.Rect(player.x + 6.5, player.y, 25, 25)
-    pg.draw.rect(screen, WCOLOR, missile)
-<<<<<<< HEAD
-=======
-    #print("Missile créé")
->>>>>>> 43b2be60d7dde168a30789d2878291348a10a792
-
 def clamp(WIDTH, HEIGHT, r):
     r.left = max(0, r.left); r.top = max(0, r.top)
     r.right = min(WIDTH, r.right); r.bottom = min(HEIGHT, r.bottom)
-
-# class MyClass:
-#     def __init__(self):
-#         self.spawned_time = time.time()
-
-#     def can_destroy(self):
-#         return time.time() > self.spawned_time + 6  # replace '6' with the seconds the class will exist for
-
-# my_instance = MyClass()
-# while True:
-#     if my_instance.can_destroy():
