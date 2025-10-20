81107e3d28783c5621989ac7eae7f2c4d36f7017
Phosnay <nils.romano@gmail.com>
2025-10-20 16:10:34 +0200

    Ajout des pv/gameover (à revoir) + quelques images

--- fonctions/vaisseau.py
+++ fonctions/vaisseau.py
@@ -38,3 +38,23 @@ def creerMissilesVaisseau():
     Missile = pg.USEREVENT + 1 #crée un événement personnalisé avec un identifiant unique
     pg.time.set_timer(Missile, 1000) #Création d'un missile par seconde
     return Missile
+
+def pv_vaisseau(pv_joueur, screen, fheart, eheart, gameover_img):
+    if pv_joueur == 3:
+        screen.blit(fheart, (10,10))
+        screen.blit(fheart, (60,10))
+        screen.blit(fheart, (110,10))
+    elif pv_joueur == 2:
+        screen.blit(fheart, (10,10))
+        screen.blit(fheart, (60,10))
+        screen.blit(eheart, (110,10))
+    elif pv_joueur == 1:
+        screen.blit(fheart, (10,10))
+        screen.blit(eheart, (40,10))
+        screen.blit(eheart, (70,10))
+    elif pv_joueur <= 0:
+        screen.blit(eheart, (10,10))
+        screen.blit(eheart, (40,10))
+        screen.blit(eheart, (70,10))
+        screen.blit(gameover_img, (0,0))
+        pg.display.flip()
