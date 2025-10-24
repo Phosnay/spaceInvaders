9838371ce8089c78411269999c2232f7476809ee
Muriel Bullot <murielbullot@hotmail.fr>
2025-10-24 15:22:46 +0200

    création de la classe player qui fonctionne

--- classes/classPlayer.py
+++ classes/classPlayer.py
@@ -21,3 +21,17 @@ class Player(pg.sprite.Sprite):
         self.rect = self.image.get_rect(topleft=(x, y))     # associer l'image à un rectangle dont le coin supérieur gauche est en x, y
         self.powerPlayer = powerPlayer
         self.speed = speed
+
+    def update(self, WIDTH, HEIGHT):
+        """ 
+        Applique un déplacement selon les touches enfoncées (flèches ou d,q,z,s)
+        """
+        k = pg.key.get_pressed()                                            # retourne l'état actuel de toutes les touches du clavier
+        dx = (k[pg.K_RIGHT] or k[pg.K_d]) - (k[pg.K_LEFT] or k[pg.K_q])     # k[pg.K_RIGHT] == 1 si la touche flèche droite est enfoncée sinon vaut 0
+        dy = (k[pg.K_DOWN]  or k[pg.K_s]) - (k[pg.K_UP]   or k[pg.K_z])     # dy vaut +1 vers le bas, -1 vers le haut, 0 sinon
+
+        self.rect.x += self.speed * dx                         # selon la vitesse (speed) définie automatiquement dans def __init__
+        self.rect.y += self.speed * dy                         # selon la vitesse (speed) définie automatiquement dans def __init__
+
+        self.rect.x = max(0, min(self.rect.x, WIDTH - self.rect.width))
+        self.rect.y = max(0, min(self.rect.y, HEIGHT - self.rect.height))
