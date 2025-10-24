1a7afa1c413b4275a0c405496feba8f14f029d9c
Muriel Bullot <murielbullot@hotmail.fr>
2025-10-24 15:56:23 +0200

    retrait du code mis en commentaire

--- fonctions/vaisseau.py
+++ fonctions/vaisseau.py
@@ -1,20 +1,6 @@
 import pygame as pg, sys                # sys = module qui permet d'interagir avec Python (sys.exit())
 import time
 
-def move_and_collide(r, dx, dy, speed, dt, walls):
-    # X
-    r.x += int(dx * speed * dt)
-    for w in walls:
-        if r.colliderect(w):
-            if dx > 0:  r.right = w.left
-            elif dx < 0: r.left  = w.right
-    # Y
-    r.y += int(dy * speed * dt)
-    for w in walls:
-        if r.colliderect(w):
-            if dy > 0:  r.bottom = w.top
-            elif dy < 0: r.top   = w.bottom
-
 def missilesPlayer(screen, missiles):
     # Mise à jour des missiles
     for m in missiles[:]:
